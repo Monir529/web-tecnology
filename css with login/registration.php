be1c820cff7a4cd00dd7a89ab877dcba0210850a
Monir529 <60336106+Monir529@users.noreply.github.com>
2020-03-14 15:58:10 +0600

    Add files via upload

---
+++ css with login/registration.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+	<title>registration</title>
+</head>
+<body>
+<header>
+
+  <h1>Xcompany</h1>
+  <ul>
+
+  <li><a href="login.php">Login</a></li>
+  <li><a href="hello.html">Home</a></li>
+  <li><a href="registration.php">registration</a></li>
+</ul>
+</header>
+
+<article>
+<form>
+
+
+  Name*:
+  <input type="text" name="firstname"><br>
+   Email*:
+   <input type="text" name="Email"><br>
+  User  Name*:
+  <input type="text" name="username"><br>
+   password:
+  <input type="password" name="password"><br>
+
+  Confirm password:
+  <input type="password" name="cpassword"><br>
+
+  Gender<br>
+   <input type="radio"  name="gender" value="male"> Male<br>
+  <input type="radio" name="gender" value="female">
+  Female<br>
+  <input type="radio"  name="gender" value="other">
+Other<br>
+Date OF Birth<br>
+  <input type="date" name="bday"><br><br>
+<input type="submit" value="submit">
+<input type="button" value="button">Reset
+
+</form>
+</article>
+<footer>
+<p>copyright @ 2020<p>
+</footer>
+
+</body>
+</html>
